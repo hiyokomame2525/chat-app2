ef8ad2f148a76fd2c7fe7aa6bbde80167b634f7f
hiyokomame2525 <5656mamesiba@gmail.com>
2023-12-28 21:47:43 +0900

    Active Storageの導入

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module ChatApp
     # in config/environments, which are processed later.
     #
     config.time_zone = "Tokyo"
+    config.active_storage.variant_processor = :mini_magick
+
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.stylesheets false
